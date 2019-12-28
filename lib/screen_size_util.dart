3f2b4658574082658cc45eb164459a71978cd3bd
Johann Feser <johannfeser@gmail.com>
2019-12-28 18:04:40 +0100

    Update screen_size_util.dart

--- lib/screen_size_util.dart
+++ lib/screen_size_util.dart
@@ -11,27 +11,26 @@ class ResponsiveBuilder extends StatelessWidget {
 
   const ResponsiveBuilder({
     Key key,
-    @required this.extraLarge,
+    this.extraLarge,
     this.large,
     this.medium,
     this.small,
-    this.extraSmall,
+    @required this.extraSmall,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     MediaQueryData data = MediaQuery.of(context);
-    if (data.isLarge && large != null) {
+    if (data.isExtraLarge && extraLarge != null) {
+      return extraLarge(context);
+    } else if (data.isLarge && large != null) {
       return large(context);
     } else if (data.isMedium && medium != null) {
       return medium(context);
     } else if (data.isSmall && small != null) {
       return small(context);
-    } else if (data.isExtraSmall && extraSmall != null) {
-      return extraSmall(context);
-    } else {
-      return extraLarge(context);
     }
+    return extraSmall(context);
   }
 }
 
@@ -42,11 +41,11 @@ extension SimpleMediaQueryData on MediaQueryData {
 
   bool get isExtraSmall => width < 576;
 
-  bool get isSmall => width >= 576;
+  bool get isSmall => width >= 576; //width < 768;
 
-  bool get isMedium => width >= 768;
+  bool get isMedium => width >= 768; //width < 992;
 
-  bool get isLarge => width >= 992;
+  bool get isLarge => width >= 992; //width < 1200;
 
   bool get isExtraLarge => width >= 1200;
 
